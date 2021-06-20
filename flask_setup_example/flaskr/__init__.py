b517acacc8cac80f9a943e6a0867a417763f7288
Beverly Thompson <blt1339@gmail.com>
2021-06-20 14:15:01 -0400

    Flaskr work

--- flask_setup_example/flaskr/__init__.py
+++ flask_setup_example/flaskr/__init__.py
@@ -2,9 +2,13 @@
 from flask import Flask, jsonify
 # Define the create_app function
 def create_app(test_config=None):
- # Create and configure the app
- # Include the first parameter: Here, __name__is the name of the current Python module.
- app = Flask(__name__)
+    # Create and configure the app
+    # Include the first parameter: Here, __name__is the name of the current Python module.
+    app = Flask(__name__)
 
- # Return the app instance
- return app
+    @app.route('/')
+    def hello_world():
+        return jsonify({'message':'Hello, World!'})
+
+    # Return the app instance
+    return app
